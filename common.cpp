9fcb4134c83b99af82d56ca7afd1055fc2a2a452
Константин <kosinosweb@playrix.com>
2022-10-01 15:21:14 +0500

    Initiate project

---
+++ common.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+string customToLower(string word)
+{
+
+	for (unsigned int i = 0; i < word.length(); i++)
+		if (word[i] == '¨')
+		{
+			word[i] += 16;
+		}
+		else if (word[i] >= 'À' && word[i] <= 'ß')
+		{
+			word[i] += 32;
+		}
+
+	return word;
+}
+
+char customToUpper(char letter)
+{
+	if (letter == '¸')
+	{
+		letter -= 16;
+	}
+	else if (letter >= 'à' && letter <= 'ÿ')
+	{
+		letter -= 32;
+	}
+
+	return letter;
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+}
