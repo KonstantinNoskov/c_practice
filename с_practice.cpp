7201252931ad8af41518a0e29dabe8f52f285524
Константин <kosinosweb@playrix.com>
2022-10-03 21:57:29 +0500

    Add project files.

---
+++ с_practice.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+string customToLower(string word)
+{
+
+	for (unsigned int i = 0; i < word.length(); i++)
+		if (word[i] == 'Ё')
+		{
+			word[i] += 16;
+		}
+		else if (word[i] >= 'А' && word[i] <= 'Я')
+		{
+			word[i] += 32;
+		}
+
+	return word;
+}
+
+char customToUpper(char letter)
+{
+	if (letter == 'ё')
+	{
+		letter -= 16;
+	}
+	else if (letter >= 'а' && letter <= 'я')
+	{
+		letter -= 32;
+	}
+
+	return letter;
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Это c_practice" << endl;
+}
