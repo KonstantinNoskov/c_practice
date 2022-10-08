b002c9e340e370fc2ec61710ebc06ebf9adad5ce
Константин <kosinosweb@playrix.com>
2022-10-08 20:39:48 +0500

    132

--- RecursiveFunctions/recursiveFunctions/recursiveFunctions.cpp
+++ RecursiveFunctions/recursiveFunctions/recursiveFunctions.cpp
@@ -4,43 +4,33 @@
 #include <windows.h>
 using namespace std;
 
-/*  Задача:
 
-    Сложить все цифры заданного числа. Если полученная сумма не является однозначным числом,
-    то цифры числа снова складываются и так до тех пор, 
-    пока не будет получено однозначное число.
-*/
+bool Prim(int j)
+{
+    int k = 2; 
 
-
-unsigned long F(unsigned long num) 
-{   
-    unsigned long
-        sum = 0; // Сумма всех цифр числа
-
-    while (num > 0) // Повторяем итерации пока не сложим все цифры
-    {   
-        sum += num % 10; // Берем самую правую цифру и плюсуем её к сумме
-       
-        cout << "Сумма итерации: " << sum << endl;
-        
-        num /= 10; // Отбрасываем число, которое только что плюсовали
+    while (k * k <= j && j % k != 0)
+    {
+        k = NextPrim(k);
     }
 
-    while (sum > 9) // Получившийся выше результат суммы снова плюсуем той же функцией пока результат - не однозначное число.
-    {
-        return F(sum);
-    } 
-    return sum;
+    return (j % k == 0) ? false : true;
 }
 
+
+bool isPrim(int j)
+{
+    int k = 2;
+
+    return (j % k == 0) ? false : true;
+}
 
 int main()
 {   
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    cout << F(288) << endl;
-   
+    cout << isPrim(21) << endl;
 }
 
 
